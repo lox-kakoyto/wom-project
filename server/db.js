21ce9b2675e8bca76e198656c67485185aa7a376
lox-kakoyto <heawen46@gmail.com>
2025-11-26 02:11:29 +0400

    Update db config for Render

--- server/db.js
+++ server/db.js
@@ -1,12 +1,26 @@
+
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-});
+const isProduction = process.env.NODE_ENV === "production";
+
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: connectionString,
+        ssl: {
+          rejectUnauthorized: false, // Обязательно для Render
+        },
+      }
+    : {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+      }
+);
 
 module.exports = pool;
